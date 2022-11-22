259b7a0912ab2ea06cca13566b18f88741b2c56b
Facundo Rauber <46487613+FacundoRauber@users.noreply.github.com>
2022-11-22 19:12:39 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+require("rootpath")();
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const config = require("config.json");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('tiny'));
+morgan(':method :url :status :res[content-length] - :response-time ms');
+
+
+app.get("/", function (req, res) {
+    res.send("SIS-SILICON ONLINE");
+});
+
+const userCont = require("userController.js");
+app.use("/api/usuario",userCont);
+
+
+
+app.listen(config.server.port, function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log(`Server iniciado en puerto:${config.server.port}`);
+    }
+});
